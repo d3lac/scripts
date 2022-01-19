a0f8fdc7a35a657c51afe8d9fa4331f7b72f4e02
d3lac <75080720+d3lac@users.noreply.github.com>
2022-01-19 20:44:44 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,3 +2,5 @@
 curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo docker --version
+sudo docker-compose --version
