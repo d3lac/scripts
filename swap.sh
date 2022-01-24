5ed5f06a83967a6fbeb1bfea8c17fdbeec21224f
d3lac <75080720+d3lac@users.noreply.github.com>
2022-01-24 19:58:17 +0530

    swap creation
    Adds 2GB swap

---
+++ swap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo swapon --show
+#adds 2GB swap
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+sudo free -h
