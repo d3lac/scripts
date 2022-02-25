a2276f2d377b53103828e13ee1f28e4412c256b1
d3lac <75080720+d3lac@users.noreply.github.com>
2022-02-25 20:40:06 +0530

    Updated

--- swap.sh
+++ swap.sh
@@ -5,6 +5,7 @@ sudo swapon --show
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
+sudo swapon /swapfile
 echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
 sudo free -h
