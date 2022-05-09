19ea6ea33fecaad4862efc03df590f723a4bb9c8
d3lac <75080720+d3lac@users.noreply.github.com>
2022-05-09 13:53:59 +0530

    docker and docker-compose
    docker and docker-compose latest version

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #curl -LsO https://raw.githubusercontent.com/d3lac/scripts/main/docker.sh; bash docker.sh
 curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
 sudo docker --version
 sudo docker-compose --version
