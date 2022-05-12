a52ec860b9d339daa2c0bc1bcdba6b9fa2b85b68
d3lac <75080720+d3lac@users.noreply.github.com>
2022-05-12 20:53:05 +0530

    Install docker and docker compose
    Install docker and docker compose

--- docker.sh
+++ docker.sh
@@ -4,5 +4,7 @@ curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 sudo chmod 755 $DESTINATION
-sudo docker --version
-sudo docker-compose --version
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo docker version
+sudo docker compose version
