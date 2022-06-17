05b8948776c42220d2d817e239fdd90c5adce811
d3lac <75080720+d3lac@users.noreply.github.com>
2022-06-17 18:31:02 +0530

    Docker and Docker compose install
    Docker and Docker compose install

--- docker.sh
+++ docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #curl -LsO https://raw.githubusercontent.com/d3lac/scripts/main/docker.sh; bash docker.sh
 curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh
-DESTINATION=/usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
-sudo chmod 755 $DESTINATION
+#DESTINATION=/usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+#sudo chmod 755 $DESTINATION
+sudo apt install -y docker-compose-plugin
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker version
-sudo docker compose --version
+sudo docker compose version
